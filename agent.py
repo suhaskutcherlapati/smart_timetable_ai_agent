7ce73375ed6f7317975594e883f5afa2b5dbb1df
suhaskutcherlapati <suhaskutcherlapati@gmail.com>
2026-03-29 18:13:50 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,39 +1,38 @@
+import os
 from langchain.tools import tool
-from langchain.agents import initialize_agent
+from langchain.agents import initialize_agent, AgentType
 from langchain_openai import ChatOpenAI
-
 from calendar_tools.create_event import create_event
 from calendar_tools.view_events import get_events
-
-
-@tool
-def create_calendar_event(text: str):
-    """Create a calendar event."""
-    return "Event created"
-
-
-@tool
-def view_calendar_events(text: str):
-    """View upcoming events."""
-    return get_events()
-
+from dotenv import load_dotenv
+load_dotenv()
+print("API KEY:", os.getenv("OPENAI_API_KEY"))
 
 def get_agent():
-
     llm = ChatOpenAI(
-        temperature=0
+        model="gpt-4o-mini",
+    
     )
-
-    tools = [
-        create_calendar_event,
-        view_calendar_events
-    ]
+    tools = [create_calendar_event, view_calendar_events]
 
     agent = initialize_agent(
         tools,
         llm,
-        agent="zero-shot-react-description",
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True
     )
 
     return agent
+
+
+@tool
+def create_calendar_event(text: str):
+    """Create a calendar event from natural language input."""
+    return create_event(text)
+
+
+@tool
+def view_calendar_events(text: str):
+    """View all upcoming events."""
+    return get_events()
+
