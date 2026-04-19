971312cc4e7f685bf8c979fb4bb10173474f1fb1
suhaskutcherlapati <suhaskutcherlapati@gmail.com>
2026-04-19 22:15:30 +0530

    update study

--- study_planner.py
+++ study_planner.py
@@ -1,41 +1,41 @@
-def create_study_plan(subjects, hours_per_day):
-    plan = []
-    
-    per_subject = hours_per_day // len(subjects)
+import json
 
-    for subject in subjects:
-        plan.append({
-            "subject": subject,
-            "hours": per_subject
-        })
+FILE = "storage/class_schedule.json"
 
-    return plan
-def study_with_breaks(total_hours):
-    sessions = []
+def load_schedule():
+    try:
+        with open(FILE, "r") as f:
+            return json.load(f)
+    except:
+        return []   # ✅ MUST be list
 
-    for i in range(total_hours * 2):  # 25min study + 5min break
-        sessions.append("📚 Study 25 mins")
-        sessions.append("☕ Break 5 mins")
-
-    return sessions
-def create_study_plan(subjects, hours_per_day):
-    plan = []
-    per_subject = hours_per_day // len(subjects)
-
-    for subject in subjects:
-        plan.append({
-            "subject": subject.strip(),
-            "hours": per_subject
-        })
-
-    return plan
+def save_schedule(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
+        
 
 
-def study_with_breaks(total_hours):
-    sessions = []
+def view_classes():
+    return load_schedule()
+def check_conflict(new_class, schedule):
+    for c in schedule:
+        if c["day"] == new_class["day"] and c["time"] == new_class["time"]:
+            return True
+    return False
+def add_class(title, day, time, type_class):
+    schedule = load_schedule()
 
-    for i in range(total_hours * 2):
-        sessions.append("📚 Study 25 mins")
-        sessions.append("☕ Break 5 mins")
+    new_class = {
+        "title": title,
+        "day": day,
+        "time": time,
+        "type": type_class
+    }
 
-    return sessions
+    if check_conflict(new_class, schedule):
+        return "❌ Conflict detected!"
+
+    schedule.append(new_class)
+    save_schedule(schedule)
+
+    return "✅ Class added"
